f9737d3e99ada3d911b24b7c8d3a3e1706b8fd1b
Paul Landes <landes@mailc.net>
2020-05-24 18:25:53 -0500

    cleanup; sub

--- src/python/setup.py
+++ src/python/setup.py
@@ -5,7 +5,6 @@ su = SetupUtil(
     setup_path=Path(__file__).parent.absolute(),
     name="zensols.deepnlp",
     package_names=['zensols', 'resources'],
-    # package_data={'': ['*.html', '*.js', '*.css', '*.map', '*.svg']},
     description='WRITE ME',
     user='plandes',
     project='deepnlp',
