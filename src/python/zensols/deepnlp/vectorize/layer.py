72e50b25abea6c62cd9f32e518400a4938cdce6c
Paul Landes <landes@mailc.net>
2021-04-02 12:57:28 -0500

    fix debug statements

--- src/python/zensols/deepnlp/vectorize/layer.py
+++ src/python/zensols/deepnlp/vectorize/layer.py
@@ -12,7 +12,7 @@ import torch
 from torch import Tensor
 from torch import nn
 from zensols.persist import persisted, Deallocatable, Primeable
-from zensols.deeplearn.model import BaseNetworkModule
+from zensols.deeplearn.model import BaseNetworkModule, DebugModule
 from zensols.deeplearn.layer import MaxPool1dFactory
 from zensols.deeplearn.vectorize import FeatureContext, TensorFeatureContext
 from zensols.deepnlp import TokensContainer, FeatureSentence, FeatureDocument
@@ -24,7 +24,7 @@ from . import TokenContainerFeatureVectorizer
 logger = logging.getLogger(__name__)
 
 
-class EmbeddingLayer(nn.Module, Deallocatable):
+class EmbeddingLayer(DebugModule, Deallocatable):
     """A class used as an input layer to provide word embeddings to a deep neural
     network.
 
@@ -33,7 +33,7 @@ class EmbeddingLayer(nn.Module, Deallocatable):
     (i.e. from directly deallocating and then from the factory).
 
     **Implementation note**: No datacasses are usable since pytorch is picky
-      about initialization order.
+    about initialization order.
 
     """
     def __init__(self, feature_vectorizer: TokenContainerFeatureVectorizer,
@@ -84,13 +84,13 @@ class WordVectorEmbeddingLayer(EmbeddingLayer):
         self.num_embeddings = embed_model.matrix.shape[0]
         self.vecs = embed_model.to_matrix(self.torch_config)
         if self.trainable:
-            logger.debug('cloning embedding for trainability')
+            self._debug('cloning embedding for trainability')
             self.vecs = torch.clone(self.vecs)
         else:
-            logger.debug('layer is not trainable')
+            self._debug('layer is not trainable')
             self.requires_grad = False
-        logger.debug(f'setting tensors: {self.vecs.shape}, ' +
-                     f'device={self.vecs.device}')
+        self._debug(f'setting tensors: {self.vecs.shape}, ' +
+                    f'device={self.vecs.device}')
         self.emb = nn.Embedding.from_pretrained(self.vecs)
         self.emb.freeze = self.trainable
 
@@ -104,16 +104,16 @@ class WordVectorEmbeddingLayer(EmbeddingLayer):
     def state_dict(self, destination=None, prefix='', *args, **kwargs):
         state = super().state_dict(destination, prefix, *args, **kwargs)
         if logger.isEnabledFor(logging.DEBUG):
-            logger.debug(f'state_dict: trainable: {self.trainable}')
+            self._debug(f'state_dict: trainable: {self.trainable}')
         if not self.trainable:
             emb_key = self._get_emb_key(prefix)
             if logger.isEnabledFor(logging.DEBUG):
-                logger.debug(f'state_dict: embedding key: {emb_key}')
+                self._debug(f'state_dict: embedding key: {emb_key}')
             if emb_key is not None:
                 arr = state[emb_key]
                 if arr is not None:
                     if logger.isEnabledFor(logging.DEBUG):
-                        logger.debug(f'state_dict: emb state: {arr.shape}')
+                        self._debug(f'state_dict: emb state: {arr.shape}')
                     assert arr.shape == self.embed_model.matrix.shape
                 state[emb_key] = None
         return state
@@ -122,7 +122,7 @@ class WordVectorEmbeddingLayer(EmbeddingLayer):
         if not self.trainable:
             emb_key = self._get_emb_key(prefix)
             if logger.isEnabledFor(logging.DEBUG):
-                logger.debug(f'load_state_dict: {emb_key}')
+                self._debug(f'load_state_dict: {emb_key}')
             if emb_key is not None:
                 state_dict[emb_key] = self.vecs
         super()._load_from_state_dict(state_dict, prefix, *args, **kwargs)
@@ -134,14 +134,14 @@ class WordVectorEmbeddingLayer(EmbeddingLayer):
                 em = '<deallocated>'
                 if hasattr(self, 'embed_model'):
                     em = self.embed_model.name
-                logger.debug(f'deallocating: {em} and {type(self.emb)}')
+                self._debug(f'deallocating: {em} and {type(self.emb)}')
             del self.emb
             del self.embed_model
 
     def forward(self, x: Tensor) -> Tensor:
         if logger.isEnabledFor(logging.DEBUG):
-            logger.debug(f'forward: {x.shape}, device: {x.device} = ' +
-                         f'{BaseNetworkModule.device_from_module(self.emb)}')
+            self._debug(f'forward: {x.shape}, device: {x.device} = ' +
+                        f'{BaseNetworkModule.device_from_module(self.emb)}')
         return self.emb.forward(x)
 
 
@@ -160,7 +160,7 @@ class BertEmbeddingLayer(EmbeddingLayer):
         super().__init__(
             *args, embedding_dim=embed_model.vector_dimension, **kwargs)
         self.embed_model = embed_model
-        logger.debug(f'config pool: {max_pool}')
+        self._debug(f'config pool: {max_pool}')
         if max_pool is not None:
             fac = MaxPool1dFactory(W=self.embedding_dim, **max_pool)
             self.pool = fac.max_pool1d()
@@ -173,9 +173,22 @@ class BertEmbeddingLayer(EmbeddingLayer):
         if hasattr(self, 'embed_model'):
             del self.embed_model
 
-    def forward(self, x: Tuple[str]) -> Tensor:
+    def forward(self, sents: Tensor) -> Tensor:
         if logger.isEnabledFor(logging.DEBUG):
-            logger.debug(f'forward: {x.shape}')
+            self._shape_debug('forward', sents)
+
+        self._bail()
+        emb = self.embed_model.transform(sents)
+        diff = self.token_length - emb.shape[0]
+        if diff > 0:
+            zeros = self.zeros
+            emb = torch.cat((torch.stack([zeros] * diff), emb))
+        elif diff < 0:
+            emb = emb[0:diff]
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.debug(f'diff: {diff}, emb shape: {emb.shape}')
+        mats.append(emb)
+
         if self.pool is not None:
             x = self.pool(x)
         return x
@@ -298,29 +311,27 @@ class BertSentenceFeatureVectorizer(SentenceFeatureVectorizer):
         else:
             doc: FeatureDocument = container
             sents = doc.sents
-        # sent_strs = tuple(map(lambda s: s.text, sents))
-        # if logger.isEnabledFor(logging.DEBUG):
-        #     logger.debug(f'encoded {len(sent_strs)} sentence(s)')
-        # return BertFeatureContext(self.feature_id, sent_strs)
-
         sent_toks = tuple(map(self.embed_model.tokenize, sents))
-
         return BertFeatureContext(self.feature_id, sent_toks)
 
     def _decode(self, context: BertFeatureContext) -> Tensor:
+        print('DECO-----')
         mats = []
         sent: Tokenization
         for sent in context.sentences:
             if logger.isEnabledFor(logging.DEBUG):
                 logger.debug(f'decoding {sent} ({type(sent)})')
-            emb = self.embed_model.transform(sent)
-            diff = self.token_length - emb.shape[0]
-            if diff > 0:
-                zeros = self.zeros
-                emb = torch.cat((torch.stack([zeros] * diff), emb))
-            elif diff < 0:
-                emb = emb[0:diff]
-            if logger.isEnabledFor(logging.DEBUG):
-                logger.debug(f'diff: {diff}, emb shape: {emb.shape}')
-            mats.append(emb)
+            #mats.append(sent.to_tensor())
         return torch.stack(mats)
+
+        #     emb = self.embed_model.transform(sent)
+        #     diff = self.token_length - emb.shape[0]
+        #     if diff > 0:
+        #         zeros = self.zeros
+        #         emb = torch.cat((torch.stack([zeros] * diff), emb))
+        #     elif diff < 0:
+        #         emb = emb[0:diff]
+        #     if logger.isEnabledFor(logging.DEBUG):
+        #         logger.debug(f'diff: {diff}, emb shape: {emb.shape}')
+        #     mats.append(emb)
+        # return torch.stack(mats)
