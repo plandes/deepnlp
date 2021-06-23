48d8562aafab8a0c305b3c0844006f7cb7cbb0ef
Paul Landes <landes@mailc.net>
2021-06-23 16:29:45 -0500

    whitespace

--- src/python/zensols/deepnlp/__init__.py
+++ src/python/zensols/deepnlp/__init__.py
@@ -2,6 +2,7 @@
 
 """
 
+
 def init(*args, **kwargs):
     """Initalize the deep NLP system, which includes a call to the PyTorch system.
     Arguments are passed on to :meth:`~zensols.deeplearn.TorchConfig.init`
