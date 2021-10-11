80c7381ed0c8d2a8b37f88c0f474c421d23a1457
Paul Landes <landes@mailc.net>
2021-10-10 21:18:06 -0500

    fix proto

--- example/clickbate/run.py
+++ example/clickbate/run.py
@@ -11,7 +11,7 @@ if (__name__ == '__main__'):
 
 CliHarness(
     app_config_resource='resources/app.conf',
-    proto_args='-c models/transformer.conf proto',
+    proto_args='-c models/transformer.conf traintest',
     proto_factory_kwargs={'reload_pattern': '^cb'},
     app_factory_class=FacadeApplicationFactory,
 ).run()
