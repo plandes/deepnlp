5e01f6119d51db3749c94ed156dff25367a0e928
Paul Landes <landes@mailc.net>
2022-06-02 11:06:53 -0500

    add transformer conditional config

--- example/southpark/harness.py
+++ example/southpark/harness.py
@@ -10,6 +10,6 @@ if (__name__ == '__main__'):
     from zensols.cli import CliHarness
     CliHarness(
         package_resource='sp',
-        proto_args='debug',
+        proto_args='debug --override sp_default.name=transformer_trainable',
         proto_factory_kwargs={'reload_pattern': '^sp'},
     ).run()
