4830436375aa488b1575caf3f2fbdb745e458a03
Paul Landes <landes@mailc.net>
2022-06-03 22:15:07 -0500

    move example: notebook harness works again

--- example/movie/harness.py
+++ example/movie/harness.py
@@ -1,28 +1,15 @@
 #!/usr/bin/env python
 
-from zensols.cli import ConfigurationImporterCliHarness
+from zensols import deepnlp
 
-
-def init():
-    # reset random state for consistency before any other packages are
-    # imported
-    from zensols.deeplearn import TorchConfig
-    TorchConfig.init()
-    # initialize the NLP system
-    from zensols import deepnlp
-    deepnlp.init()
-
-
-def create_harness(*args, **kwargs):
-    return ConfigurationImporterCliHarness(
-        *args,
-        src_dir_name='src',
-        proto_args='debug',
-        proto_factory_kwargs={'reload_pattern': '^(?:app|dataset|domain)'},
-        **kwargs,
-    )
+# initialize the NLP system
+deepnlp.init()
 
 
 if (__name__ == '__main__'):
-    init()
-    create_harness().run()
+    from zensols.cli import CliHarness
+    CliHarness(
+        package_resource='mr',
+        proto_args='debug',
+        proto_factory_kwargs={'reload_pattern': '^mr'},
+    ).run()
---
+++ example/movie/notebook/mngfac.py
@@ -0,0 +1,41 @@
+"""Environemnt configuration and set up: add this (deepnlp) library to the
+Python path and framework entry point.
+
+"""
+__author__ = 'Paul Landes'
+
+from pathlib import Path
+
+
+class JupyterManagerFactory(object):
+    """Bootstrap and import libraries to automate notebook testing.
+
+    """
+    def __init__(self, app_root_dir: Path = Path('..')):
+        """Set up the interpreter environment so we can import local packages.
+
+        :param app_root_dir: the application root directory
+
+        """
+        from zensols import deepnlp
+        deepnlp.init()
+        from zensols.cli import ConfigurationImporterCliHarness
+        self._harness = ConfigurationImporterCliHarness(
+            package_resource='mr',
+            root_dir=app_root_dir.absolute())
+
+    def __call__(self):
+        """Create a new ``JupyterManager`` instance and return it."""
+        from zensols.deeplearn.cli import JupyterManager
+
+        def map_args(embedding: str = None, config_name: str = 'wordvec'):
+            args = []
+            if embedding is not None:
+                args.extend(['--override', f'mr_default.name={embedding}'])
+            if config_name is not None:
+                config_file = self._harness.root_dir / f'models/{config_name}.conf'
+                args.extend(['--config', str(config_file)])
+            return args
+
+        return JupyterManager(self._harness, cli_args_fn=map_args,
+                              reduce_logging=True)
