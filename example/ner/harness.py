61e7e2c99f1a440559c4729e5c89fbc04e74eaa0
Paul Landes <landes@mailc.net>
2022-06-05 07:26:40 -0500

    ner example: massage and document

--- example/ner/harness.py
+++ example/ner/harness.py
@@ -10,8 +10,6 @@ if (__name__ == '__main__'):
     from zensols.cli import CliHarness
     CliHarness(
         package_resource='ner',
-        #proto_args='proto',
-        #proto_args='info -i config',
         proto_args={0: 'proto',
                     1: 'info -i config',
                     2: 'stats',
