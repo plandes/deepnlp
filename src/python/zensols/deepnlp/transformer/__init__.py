12761a87e06956c59e603713fc4b35e1e4aed9d3
Paul Landes <landes@mailc.net>
2022-10-12 06:25:13 +0900

    add huggingface funcs

--- src/python/zensols/deepnlp/transformer/__init__.py
+++ src/python/zensols/deepnlp/transformer/__init__.py
@@ -8,9 +8,28 @@ def suppress_warnings():
     """Suppress the ```Some weights of the model checkpoint...``` warnings from
     huggingface transformers.
 
+    :ses: :func:`normalize_huggingface_logging`
+
     """
     from transformers import logging
     logging.set_verbosity_error()
+
+
+def normalize_huggingface_logging():
+    """Make the :mod"`transformers` package use default logging.  Using this and
+    setting the ``transformers`` logging package to ``ERROR`` level logging has
+    the same effect as :meth:`suppress_warnings`.
+
+    """
+    from transformers import logging
+    logging.disable_default_handler()
+    logging.enable_propagation()
+
+
+def turn_off_huggingface_downloads():
+    """Turn off automatic model checks and downloads."""
+    import os
+    os.environ['TRANSFORMERS_OFFLINE'] = '1'
 
 
 from .domain import *
